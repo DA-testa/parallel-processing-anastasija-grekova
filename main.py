79f14f480739aedfaed384e0bb1630b8d28d6ff3
anastasija-grekova <125025406+anastasija-grekova@users.noreply.github.com>
2023-03-19 21:40:41 +0000

    try1

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 # python3
 
-def parallel_processing(n, m, data):
-    output = []
-    # TODO: write the function for simulating parallel tasks, 
-    # create the output pairs
+from queue import Queue
+import threading
+import time
 
-    return output
+count = []
+n = 0 #thread count 
+#jobIndex = 0
+activeThreads = 0
+m = 0 #job count
+data = [] #seconds per job
+    
+def jobTime(second, threadIndex, q):
+#    global jobIndex
+    global count
+    q.put(0)
+ #   global jobIndex
+    #jobIndex += 1
+    time.sleep(second)
+    print(threadIndex, count[threadIndex])
+    count[threadIndex] += second
+
+    
+    
+    if q.qsize() < m:
+        #count[threadIndex] += second
+        jobTime(data[q.qsize()], threadIndex, q)
+        #t = threading.Thread(target=jobTime, args=(data[len(jobIndex)-1], threadIndex))
+        #t.start()
+        #print(threadIndex, " ", data[i])
+
+
 
 def main():
     # TODO: create input from keyboard
@@ -13,15 +38,40 @@ def main():
     # first line - n and m
     # n - thread count 
     # m - job count
-    n = 0
-    m = 0
-
+    #n = 0
+    #m = 0
+    global m
+    global n
+    global data
+    n, m = map(int, input().split())       # 2 5
+    data = list(map(int, input().split())) # 1 2 3 4 5
     # second line - data 
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
+    #data = []
+    #output = []
+    global activeThreads
+    global count
+    queue = Queue()
+    for i in range(0, n):
+        #jobTime(data[i], i)
+        activeThreads += 1
+        count.append(0)
+        t = threading.Thread(target=jobTime, args=(data[i], i,queue))
+        t.start()
+       # t.join()
+
+        #print(i, " ", 0)
+
+        
+        
+
+    # TODO: write the function for simulating parallel tasks, 
+    # create the output pairs
+
+    
 
     # TODO: create the function
-    result = parallel_processing(n,m,data)
+    #result = parallel_processing(n,m,data)
     
     # TODO: print out the results, each pair in it's own line
 
